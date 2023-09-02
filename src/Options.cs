dc147e7d0fe462e6e49b7945ccc8e2bb184b0a33
Mavaddat Javid <5055400+mavaddat@users.noreply.github.com>
2023-09-02 05:04:08 -0400

    Update Options.cs
    Correctly point to the `.exe` when relying on the `code*.cmd` to tell us where the VS Code lives.

--- src/Options.cs
+++ src/Options.cs
@@ -22,23 +22,34 @@ namespace OpenInVsCode
             using var ps = PowerShell.Create(RunspaceMode.NewRunspace);
             ps.AddCommand("Get-Command").AddParameter("Name","code*.cmd").AddParameter("CommandType","Application").AddParameter("ErrorAction","SilentlyContinue");
             var codeCmd = ps.Invoke<ApplicationInfo>();
-            if(!ps.HadErrors && codeCmd[0] != null && codeCmd[0].GetType().GetProperty("Definition") != null && Regex.Match(FileVersionInfo.GetVersionInfo(codeCmd[0].Definition).ProductName, @"Microsoft VS Code").Success)
+
+            if(!ps.HadErrors && codeCmd[0] != null && Regex.Match(codeCmd[0].Definition, @"Microsoft VS Code").Success)
             {
-                return codeCmd[0].Definition;
-            } else {
-                // foreach path in "%localappdata%\Programs", "%programfiles%", "%programfiles(x86)%" check for "\Microsoft VS Code*\Code*.exe"
-                var paths = new string[] { Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\Programs", Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86) };
-                foreach (var path in paths)
+                string codePath = codeCmd[0].Definition;
+                for (int i = 0; i < 2; i++)
                 {
                     ps.Commands.Clear();
-                    ps.AddCommand("Resolve-Path").AddParameter("Path", Path.Combine(path, "Microsoft VS Code*","Code*.exe")).AddParameter("ErrorAction", "SilentlyContinue");
-                    var resolvedPath = ps.Invoke<PathInfo>();
-                    if(!ps.HadErrors && resolvedPath[0].Path != null && Regex.Match(FileVersionInfo.GetVersionInfo(resolvedPath[0].Path).ProductName, @"Microsoft VS Code").Success){
-                        return resolvedPath[0];
-                    }
+                    ps.AddCommand("Split-Path").AddParameter("Path", codePath).AddParameter("Parent").AddParameter("ErrorAction", "SilentlyContinue");
+                    codePath = ps.Invoke<string>();
                 }
-                return null;
+                ps.Commands.Clear();
+                ps.AddCommand("Resolve-Path").AddParameter("Path", Path.Combine(codePath, "Code*.exe")).AddParameter("ErrorAction", "SilentlyContinue");
+                codePath = ps.Invoke<PathInfo>();
+                if(!ps.HadErrors && codePath[0].Path != null && Regex.Match(FileVersionInfo.GetVersionInfo(codePath[0].Path).ProductName, @"Microsoft VS Code").Success){
+                return codePath;
             }
+            // foreach path in "%localappdata%\Programs", "%programfiles%", "%programfiles(x86)%" check for "\Microsoft VS Code*\Code*.exe"
+            var paths = new string[] { Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\Programs", Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86) };
+            foreach (var path in paths)
+            {
+                ps.Commands.Clear();
+                ps.AddCommand("Resolve-Path").AddParameter("Path", Path.Combine(path, "Microsoft VS Code*","Code*.exe")).AddParameter("ErrorAction", "SilentlyContinue");
+                var resolvedPath = ps.Invoke<PathInfo>();
+                if(!ps.HadErrors && resolvedPath[0].Path != null && Regex.Match(FileVersionInfo.GetVersionInfo(resolvedPath[0].Path).ProductName, @"Microsoft VS Code").Success){
+                    return resolvedPath[0];
+                }
+            }
+            return null;
         }
             set;
         }
